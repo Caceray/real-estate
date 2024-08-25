4a8d59d79d179165348aea57bce42dfbf6312c2e
Carlos Garay <carlos.garay@live.fr>
2024-08-26 00:58:40 +0200

    Remove hardcoded key from settings and add requirements.txt

--- simulator/settings.py
+++ simulator/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+01x%iz25&#07_l!z8@1z6c8me==ifs_5lwp#d#u6p7b7cyaqj'
+from decouple import config
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
